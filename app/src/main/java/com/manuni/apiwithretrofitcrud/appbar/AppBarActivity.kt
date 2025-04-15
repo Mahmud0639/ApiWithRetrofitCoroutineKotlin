8863c201c7a5325a59238e457c53be4f1a001b2c
mahmud <bappimatubber1997@gmail.com>
2025-04-15 12:35:29 +0600

    menu-context-menu

--- app/src/main/java/com/manuni/apiwithretrofitcrud/appbar/AppBarActivity.kt
+++ app/src/main/java/com/manuni/apiwithretrofitcrud/appbar/AppBarActivity.kt
@@ -1,8 +1,10 @@
 package com.manuni.apiwithretrofitcrud.appbar
 
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +14,7 @@ import com.manuni.apiwithretrofitcrud.R
 import com.manuni.apiwithretrofitcrud.databinding.ActivityAppBarBinding
 
 class AppBarActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityAppBarBinding
+    private lateinit var binding: ActivityAppBarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAppBarBinding.inflate(layoutInflater)
@@ -21,32 +23,61 @@ class AppBarActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         title = "App bar and menu"
 
+        /**
+         * context menu means when we click on the button or any view as long click then
+         * the menu should appear with the button or view
+         */
 
+        //registering context menu with the button
+        //if we want to register list view or recycler view we need to register that id only here
+        registerForContextMenu(binding.contextMenuBtn)
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.settings ->{
-                Toast.makeText(this,"Settings is clicked",Toast.LENGTH_SHORT).show()
+        when (item.itemId) {
+            R.id.settings -> {
+                Toast.makeText(this, "Settings is clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.about_me ->{
-                Toast.makeText(this,"about me is clicked",Toast.LENGTH_SHORT).show()
+
+            R.id.about_me -> {
+                Toast.makeText(this, "about me is clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.aboutCompany ->{
-                Toast.makeText(this,"about company is clicked",Toast.LENGTH_SHORT).show()
+
+            R.id.aboutCompany -> {
+                Toast.makeText(this, "about company is clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.exit ->{
-                Toast.makeText(this,"exit is clicked",Toast.LENGTH_SHORT).show()
+
+            R.id.exit -> {
+                Toast.makeText(this, "exit is clicked", Toast.LENGTH_SHORT).show()
                 //to exit an activity
                 finish()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        menuInflater.inflate(R.menu.menu_context,menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.exitContext ->{
+                Toast.makeText(this, "Exit is clicked", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            R.id.aboutContext ->{
+                Toast.makeText(this, "About is clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
