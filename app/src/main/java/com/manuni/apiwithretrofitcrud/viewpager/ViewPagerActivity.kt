df1e157a30652065ee238f7b48c27ea10c2dff53
mahmud <bappimatubber1997@gmail.com>
2025-04-16 22:42:30 +0600

    tablayout-customized-tablayout-3 ways-customization-in-style-theme-xml-and-programmatically

--- app/src/main/java/com/manuni/apiwithretrofitcrud/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/manuni/apiwithretrofitcrud/viewpager/ViewPagerActivity.kt
@@ -3,9 +3,11 @@ package com.manuni.apiwithretrofitcrud.viewpager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.manuni.apiwithretrofitcrud.R
 import com.manuni.apiwithretrofitcrud.databinding.ActivityViewPagerBinding
 
@@ -18,9 +20,22 @@ class ViewPagerActivity : AppCompatActivity() {
         binding = ActivityViewPagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //viewpager
         val fragments:List<Fragment> = listOf(FirstFragment(),SecondFragment())
 
         val adapter = ViewPagerAdapter(fragments,supportFragmentManager,lifecycle)
         binding.viewPager.adapter = adapter
+
+        //tab layout
+
+        //we can customize tabLayout programmatically
+        /*binding.tabLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.your_tab_background_color))
+        binding.tabLayout.setTabTextColors(ContextCompat.getColor(this,R.color.your_unselected_text_color),ContextCompat.getColor(this,R.color.your_selected_text_color))
+        binding.tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this,R.color.your_indicator_color))*/
+
+        val fragmentsArray = arrayOf("First","Second")
+        TabLayoutMediator(binding.tabLayout,binding.viewPager){ tab, position ->
+            tab.setText(fragmentsArray[position])
+        }.attach()
     }
 }
